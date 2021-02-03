a8720855acd4f0154d54de1c8e02fcb4f23d7fde
Bryce <themaughan1.0@gmail.com>
2021-02-02 17:11:31 -0700

    Fixing the syntax

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
-    }
+/*
+
+CSE310 Java Workshop - Example 1 - Solution
+
+This example will use Hello World to explore the structure of 
+a Java program including classes, main function, constructors, 
+member data, and functions
+*/
+
+public class HelloWorld {
+
+  // Member data
+  private String myName;
+
+  /* Constructor */
+  public HelloWorld(String myName) {
+      this.myName = myName;
   }
-  
+
+  public void sayHello() {
+      System.out.println("Hello World from sayHello");
+  }
+
+  public void sayHelloFromMe() {
+      // Use member data
+      System.out.println("Hello World from "+myName);
+  }
+
+  public void sayHelloFromYou(String yourName) {
+      // Use parameter
+      System.out.println("Hello World from "+yourName);
+  }
+
+  /* In REPL, it will look for the main function
+     in a class called Main.  In other tools, you don't
+     need to call the class Main. 
+  */
+  public static void main(String[] args) {
+      System.out.println("Hello World from main");
+
+      HelloWorld hello = new HelloWorld("Bob"); // Create new hello object
+      hello.sayHello();
+      hello.sayHelloFromYou("Sue");
+      hello.sayHelloFromMe();
+  }
+}
