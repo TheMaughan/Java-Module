72f3778b9e5640083c3d2a6d12d6f366671de5d2
Bryce <themaughan1.0@gmail.com>
2021-02-03 20:08:07 -0700

    Getting ready

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ This example will use Hello World to explore the structure of
 a Java program including classes, main function, constructors, 
 member data, and functions
 */
+import java.util.Scanner;
 
 public class HelloWorld {
 
@@ -31,6 +32,32 @@ public class HelloWorld {
       System.out.println("Hello World from "+yourName);
   }
 
+  public void enterNum() {
+      // This is used for simple user input:
+      int num1, num2, sum;
+      Scanner scan = new Scanner(System.in);
+      System.out.print("Enter any number: ");
+      num1 = scan.nextInt();
+      
+      System.out.println(" The number entered by user: "+num1);
+      System.out.print("Enter your second number: ");      
+      num2 = scan.nextInt();
+      System.out.println(" The second number is: "+num2);
+      scan.close();
+      // Is the number positive or negative?
+      if(num1 > 0){
+          System.out.println(num1+" is a positive number");
+      } else if(num1 < 0) {
+          System.out.println(num1+" is a negative number");
+      } else {
+          System.out.println(num1+" is neither positive or negative");
+      }
+
+      sum = num1 + num2;
+
+      System.out.println("Sum of your two entries: "+sum);
+  }
+
   /* In REPL, it will look for the main function
      in a class called Main.  In other tools, you don't
      need to call the class Main. 
@@ -42,5 +69,8 @@ public class HelloWorld {
       hello.sayHello();
       hello.sayHelloFromYou("Sue");
       hello.sayHelloFromMe();
+      hello.enterNum();
+
+      
   }
 }
