017c6655d60c16c13d959ba99ad3659d4b9c7df4
Bryce <themaughan1.0@gmail.com>
2021-02-06 00:46:45 -0700

    Receipt program finished

--- E-commerse/Order.java
+++ E-commerse/Order.java
@@ -1,13 +1,37 @@
-import Product;
+import java.util.ArrayList;
 public class Order {
     String id;
-    String[] products;
+    ArrayList<Product> products = new ArrayList<Product>();
 
-    public void getSubtotal() {
-        double orderSubtotal;
+    public Order(String i) {
+        this.id = i;
+    }
 
-        for(String[] i : products)
-            orderSubtotal += 
+    public double getSubtotal() {
+        double orderSubtotal = 0;
+        for(Product i : products)
+            orderSubtotal += i.getTotalPrice();
+        
+        return orderSubtotal;
+    }
+
+    public double getTax() {
+        return getSubtotal() * 0.065;
+    }
+
+    public double getTotal() {
+        return getSubtotal() + getTax();
+    }
+
+    public void displayReceipt() {
+        System.out.printf("Order: %s", this.id);
+        
+        for(Product i : products)
+            i.display();
+        
+        System.out.printf("\nSubtotal: $%.2f",getSubtotal());
+        System.out.printf("\nTax: $%.2f",getTax());
+        System.out.printf("\nTotal: $%.2f\n",getTotal());
     }
 
     
--- E-commerse/Product.java
+++ E-commerse/Product.java
@@ -19,21 +19,6 @@ public class Product {
 	}
 
 	public void display() {
-		System.out.printf("\n%s (%.0f) - $%.2f",this.name, this.quantity, getTotalPrice());
-	}
-
-
-
-    public static void main(String[] args) {
-		System.out.println("### Testing Products ###");
-
-		Product p1 = new Product("1238223", "Sword", 1899.99, 10);
-		System.out.printf("Id: %s",p1.id);
-		System.out.printf("\nName: %s",p1.name);
-		System.out.printf("\nPrice: %.2f",p1.price);
-		System.out.printf("\nQuantity: %f\n",p1.quantity);
-
-		p1.getTotalPrice();
-		p1.display();
+		System.out.printf("\n%s (%.0f) - $%.2f", name, quantity, getTotalPrice());
     }
 }
